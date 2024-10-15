730c8b4f3c2339ff6db48ddf39c1330627635cbe
lavender78 <lindawu8134@gmail.com>
2024-10-15 15:35:29 +0800

    feat: add document

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import {useCallback, useState, useEffect} from 'react';
 import { InstantSearch, SearchBox, InfiniteHits } from 'react-instantsearch';
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 import 'instantsearch.css/themes/satellite.css';
+import {MeiliSearch} from 'meilisearch';
 
 const { searchClient } = instantMeiliSearch(
   'https://ms-0eb0292ba9f0-14294.fra.meilisearch.io',
   '240ac85ce526ae7623867f484fee1bc1c6601647'
 );
 
-const App = () => (
-  <InstantSearch
-    indexName="movies"
-    searchClient={searchClient}
-  >
-    <SearchBox />
-    <InfiniteHits hitComponent={Hit} />
-  </InstantSearch>
-);
+const App = () => {
+  const [client, setClient] = useState(null)
+  const initClient = useCallback(() => {
+    const initClientConstant = new MeiliSearch({host: 'https://ms-0eb0292ba9f0-14294.fra.meilisearch.io', apiKey: '240ac85ce526ae7623867f484fee1bc1c6601647'})
+    setClient(initClientConstant)
+  },[])
+  const handlePost = useCallback(async () => {
+    if(!client){
+      return;
+    }
+    const documents = [
+      { id: 1, title: 'Carolize', genres: ['Romance', 'Drama'] },
+      { id: 2, title: 'Wonder Woman', genres: ['Action', 'Adventure'] },
+      { id: 3, title: 'Life of Pi', genres: ['Adventure', 'Drama'] },
+      { id: 4, title: 'Mad Max: Fury Road', genres: ['Adventure', 'Science Fiction'] },
+      { id: 5, title: 'Moana', genres: ['Fantasy', 'Action']},
+      { id: 6, title: 'Philadelphia', genres: ['Drama'] },
+    ]
+    await client.index('others').addDocuments(documents)
+      .then((res) => console.log(res))
+  },[client])
+  useEffect(() => {
+    initClient()
+  },[])
+  return (
+    <InstantSearch
+      indexName="others"
+      searchClient={searchClient}
+    >
+      <SearchBox /> <span onClick={() => handlePost()}>Post</span>
+      <InfiniteHits hitComponent={Hit} />
+    </InstantSearch>
+  )
+};
 
 const Hit = ({ hit }) => (
   <article key={hit.id}>
